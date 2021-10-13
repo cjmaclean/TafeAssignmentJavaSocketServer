42ccb6534dea0ecc4b2431da0cf87e7b492cd729
Caspian Maclean <30039802@tafe.wa.edu.au>
2021-10-13 14:40:50 +0800

    Working on the password hashing code

--- src/javasocketserver/PasswordUtilities.java
+++ src/javasocketserver/PasswordUtilities.java
@@ -22,65 +22,74 @@ import javax.crypto.spec.PBEKeySpec;
  */
 public class PasswordUtilities {
 
+    // Crypto code based on the howto at https://www.baeldung.com/java-password-hashing
     static SecureRandom random;
-      static      int iterations = 1024;
-      static  int keyLength = 128;
-    
+
+    //should work on constants vs variables.
+    // keyLength is intended constant.
+    // iterations contant for now, but stored in case we want multiple values later.
+    static int iterations = 1024;
+    static int keyLength = 128;
+
     private static void PasswordUtilities() throws NoSuchAlgorithmException, InvalidKeySpecException {
-
-        // Crypto code based on the howto at https://www.baeldung.com/java-password-hashing
-        // should move rng init to separate place
         random = new SecureRandom();
-
     }
 
-    private static void addStoredUser() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    static int keyLeng = 4;
 
-        // Crypto code based on the howto at https://www.baeldung.com/java-password-hashing
-        // should move rng init to separate place
-        SecureRandom random = new SecureRandom();
+    private static byte[] saltedHashedPassword(String password, byte[] salt, int iterations) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, keyLength);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        return hash;
+    }
+
+    private static byte[] saltedHashedPassword(String password, User user) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return saltedHashedPassword(password, user.salt, user.iterations);
+    }
+
+    private static void generateSaltAndHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+// not sure if this function will be useful. Incomplete
         byte[] salt = new byte[16];
         random.nextBytes(salt);
-
-        int iterations = 1024;
-        int keyLength = 128;
 
         KeySpec spec = new PBEKeySpec(storedPassword.toCharArray(), salt, iterations, keyLength);
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 
         byte[] hash = factory.generateSecret(spec).getEncoded();
-        storedHash = hash;
 
         User newUser = new User();
-        newUser.userName = storedUserName;
+        //newUser.userName = storedUserName;
         newUser.hash = hash;
         newUser.salt = salt;
         newUser.iterations = iterations;
 
     }
 
-    private static void addStoredUser() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    private static boolean isCorrectPassword(User user, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
-        // Crypto code based on the howto at https://www.baeldung.com/java-password-hashing
-        // should move rng init to separate place
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
+        byte[] computedHash = saltedHashedPassword(password, user);
 
-        int iterations = 1024;
-        int keyLength = 128;
+        newUser.hash == hash;
 
-        KeySpec spec = new PBEKeySpec(storedPassword.toCharArray(), salt, iterations, keyLength);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+    }
 
-        byte[] hash = factory.generateSecret(spec).getEncoded();
-        storedHash = hash;
+    private static User prepareUser(String userName, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
         User newUser = new User();
-        newUser.userName = storedUserName;
-        newUser.hash = hash;
+        newUser.userName = userName;
+
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
         newUser.salt = salt;
+
         newUser.iterations = iterations;
+
+        newUser.hash = saltedHashedPassword(password, newUser);
+
+        return newUser;
 
     }
 
